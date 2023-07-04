f7a061ff0d98d84290efafcd2e29b28e7ea86c44
Dan Druff <hamtron2000@gmail.com>
2023-07-04 12:30:33 -0400

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,56 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { Environment, OrbitControls } from "@react-three/drei"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Suspense } from "react"
+import { styled } from "styled-components"
+// import { GLTFLoader } from "three-stdLib"
+import styles from './page.module.css';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 export default function Home() {
+
+  const Model = () => {
+    // const gltf = useLoader(GLTFLoader,'/threeD/scene.gltf')
+    const gltf = useLoader(GLTFLoader,'/suzy/suzy.gltf')
+
+    return (
+      <>
+        <primitive dispose={null} object={gltf.scene} scale={0.7}/>
+      </>
+    )
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <ThreeDContainer>
+       
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <h2>HELLO?</h2>
+        <Canvas shadows dpr={[1,2]} camera={{position:[0,0,4], fov:50}}>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <ambientLight intensity={0.7} />
+          <spotLight  intensity={0.5} angle={0.1} penumbra={2} position={[10,15,10]} castShadow/>
+          
+          <Suspense fallback={null}>
+            <Model />
+            <Environment preset="apartment" background={true}/>
+          </Suspense>
+        
+          <OrbitControls autoRotate/>
+        </Canvas>
+      
+      </ThreeDContainer>
     </main>
   )
 }
+const ThreeDContainer = styled.div`
+  canvas {
+    // height: 660px !important;
+    // width: 400px !important;
+    // width:100%;
+    width:100%;
+    
+  }
+  text-align:center;
+`;
---
+++ src/app/three/page.tsx
@@ -0,0 +1,10 @@
+
+const ThreePage = () => {
+
+
+  return (
+    <div>Three Page</div>
+  )
+}
+
+export default ThreePage
