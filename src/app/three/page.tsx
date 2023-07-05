0e77bba8b342183faac1c8fa5cd9b13c4b9cb9ca
Dan Druff <hamtron2000@gmail.com>
2023-07-05 17:20:36 -0400

    vrbutton?

--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -5,18 +5,22 @@ import AnimatedBox from "../Components/AnimatedBox";
 // import CameraOrbitController from "../Components/CameraOrbitController";
 import { OrbitControls, Stats } from "@react-three/drei";
 import { Suspense, useState } from "react";
+import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 const ThreePage = () => {
-    const [testing,setTesting] = useState<boolean>(true);
+    const [testing,setTesting] = useState<boolean>(false);
 //    const testing = false;
 
   return (
     <div className={styles.container}>
-        
+        <VRButton />
     <Canvas>
+        <XR>
+            <Controllers />
+            <Hands />
         {testing ? <Stats /> : null}
         {testing ? <axesHelper visible={testing} args={[2]} /> : null}
        {testing ? <gridHelper args={[10,10]}/> : null}
-        <OrbitControls />
+        {/* <OrbitControls /> */}
         
         {/* <CameraOrbitController /> */}
         <ambientLight intensity={0.1} />
@@ -24,7 +28,7 @@ const ThreePage = () => {
         
             <AnimatedBox isTesting={testing}/>
         
-        
+            </XR>
     </Canvas>
     
     </div>
